90d05d92eaa37a491e15eb6d861fffe7aaa4fad5
Amanda Perch <perch_amanda@hotmail.com>
2023-02-09 16:04:24 -0300

    Implementa txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith('.txt'):
+        sys.stderr.write('Formato Invalido\n')
+    try:
+        with open(path_file) as file:
+            file_txt = file.read()
+            return file_txt.split('\n')
+    except FileNotFoundError:
+        sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
+    
