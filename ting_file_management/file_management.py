4ef70ad31d35d83ab131a5112da9d630f43b5dec
Amanda Perch <perch_amanda@hotmail.com>
2023-02-09 16:17:25 -0300

    Formato invalido com letra minuscula

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -3,10 +3,11 @@ import sys
 
 def txt_importer(path_file):
     if not path_file.endswith('.txt'):
-        sys.stderr.write('Formato Invalido\n')
+        sys.stderr.write("Formato inválido\n")
     try:
         with open(path_file) as file:
             file_txt = file.read()
-            return file_txt.split('\n')
+            return file_txt.split("\n")
+
     except FileNotFoundError:
         sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
