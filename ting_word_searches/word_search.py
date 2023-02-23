45179ef8610cd854d63729c6b757e35f8f5e42f3
Amanda Perch <perch_amanda@hotmail.com>
2023-02-23 16:03:14 -0300

    Função exist_word de word_search

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,23 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    exist_word = []
+    list_size = len(instance)
+
+    for _data in range(list_size):
+        search_file = instance.search(_data)
+        files = {
+            "palavra": word,
+            "arquivo": search_file["nome_do_arquivo"],
+            "ocorrencias": [],
+        }
+
+        for i, file in enumerate(search_file["linhas_do_arquivo"]):
+            if word.lower() in file.lower():
+                files["ocorrencias"].append({"linha": i + 1})
+
+        if files["ocorrencias"]:
+            exist_word.append(files)
+
+    return exist_word
 
 
 def search_by_word(word, instance):
